03cd88655b59c5f79ef1fa588491d62a3678fadc
Ritaja Sengupta <ritajasengupta@Ritajas-MBP.fritz.box>
2020-04-26 17:58:46 +0200

    adapting to reference

--- src/main/java/ai/knowledge/raw/service/kafka/ConsumerService.java
+++ src/main/java/ai/knowledge/raw/service/kafka/ConsumerService.java
@@ -1,7 +1,9 @@
 package ai.knowledge.raw.service.kafka;
 
 import ai.knowledge.news.raw.NewsArticle;
+import ai.knowledge.raw.model.News;
 import ai.knowledge.raw.service.orientdb.WriterClientService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.apachecommons.CommonsLog;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 @CommonsLog(topic = "Raw Consumer Logger")
@@ -29,14 +33,21 @@ public class ConsumerService {
     @Autowired
     private KafkaListenerEndpointRegistry registry;
 
+    private ObjectMapper objectMapper = new ObjectMapper();
+
     private WriterClientService writerClientService;
 
 
-    @KafkaListener(id = "RawConsumer", topics = "${topic.name}", autoStartup =
-            "false")
+    @KafkaListener(id = "Java-RawConsumer", topics = "${topic.name}",
+            autoStartup = "false")
     public void consume(ConsumerRecord<String, NewsArticle> record) {
         log.info(String.format("Consumed message -> %s", record.value()));
-        writerClientService.createVertex(record);
+        // each record is ConsumerRecord<String, NewsArticle>
+        // needs explicit cast/variable otherwise library below not works
+        News news = mapToNews(record.value().toString());
+        if (news != null) {
+            writerClientService.createVertex(news, record.key());
+        }
     }
 
     // delay start, read: https://docs.spring.io/spring-kafka/reference/html/#kafkalistener-lifecycle
@@ -45,11 +56,23 @@ public class ConsumerService {
         // initialize the db connection before starting the consumer
         writerClientService = new WriterClientService(orientUrl, orientDB,
                 orientWriteUser, orientWritePass);
-        this.registry.getListenerContainer("RawConsumer").start();
+        this.registry.getListenerContainer("Java-RawConsumer").start();
     }
 
     public void stopConsumer() {
-        this.registry.getListenerContainer("RawConsumer").stop();
+        this.registry.getListenerContainer("Java-RawConsumer").stop();
+    }
+
+    private News mapToNews(String jsonString) {
+        //Car car = objectMapper.readValue(json, Car.class);
+        News news = null;
+        try {
+            news = objectMapper.readValue(jsonString, News.class);
+        } catch (IOException e) {
+            log.error(String.format("Could not convert Kafka message to News " +
+                    "data type, jsonString: %s", jsonString));
+        }
+        return news;
     }
 
 
